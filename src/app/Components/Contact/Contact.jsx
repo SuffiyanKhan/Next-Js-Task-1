b86c41dbd19f37c09faf51e28a4b3ed420e06758
Suffiyan Khan <suffiyanahmed804092@gmail.com>
2024-05-21 18:57:30 +0500

    done

---
+++ src/app/Components/Contact/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <>
+    <div className="container mt-5">
+        <h3 className="text-center">Contact</h3>
+        <div className="row mt-5">
+            <div className="">
+              
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Contact
---
+++ src/app/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='container-fluid bg-light text-dark'>
+            <div className="row">
+                <div className="text-center ">
+                    <p className='mt-3'> ©2024 Created by Ant UED</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
